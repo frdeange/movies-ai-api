485c51d3abaa87ff072a7469e8ba6c1639906883
Kiko de Angel <frdeange@microsoft.com>
2024-10-10 14:17:59 +0000

    Navigate into the web to extract all the cinemas from pages

--- app.py
+++ app.py
@@ -12,207 +12,212 @@ swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
     config={
-        'app_name': "API de Cines y Películas"
+        'app_name': "Cinema and Movies API"
     }
 )
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
-def get_cines_data():
-    url = "https://www.sensacine.com/cines/ciudades-72368/"
+def get_cinemas_data():
+    # Base URL of the web page
+    base_url = "https://www.sensacine.com/cines/provincias-27410/"
 
-    # Gets the HTML content of the page
-    response = requests.get(url)   
-    
-    # Checks if the request was successful
-    if response.status_code != 200:
-        return None
-    
-    # Parses the HTML content
-    soup = BeautifulSoup(response.content, 'lxml')
+    # Make the HTTP request to the web page
+    headers = {'User-Agent': 'Mozilla/5.0'}
 
-    # Finds all the div elements with the class 'meta meta-theater'
-    cines = soup.find_all('div', class_='meta meta-theater')
-    
-    # Checks if any cines were found
-    if not cines:
-        return None
-    
-    cines_data = []
-    
-    # Iterates over the cines
-    for cine in cines:
-        # Finds the h2 element with the class 'title'
-        nombre_cine = cine.find('h2', class_='title')
-        # Finds the address element with the class 'address address-without-acces'
-        direccion_cine = cine.find('address', class_='address address-without-acces')
-        # Finds the a element with the href attribute
-        enlace_cine = cine.find('a', href=True)
-        # num salas
-        num_salas = cine.find(class_='screen-number')
+    # Get the total number of pages
+    response = requests.get(base_url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Checks if all the required elements were found
-        if nombre_cine and direccion_cine and enlace_cine:
-            # Creates a dictionary with the data
-            cine_data = {
-                "nombre": nombre_cine.text.strip(),
-                "direccion": direccion_cine.text.strip(),
-                "url": "https://www.sensacine.com" + enlace_cine['href'],
-                "num_salas": num_salas.text.strip()
-            }            
-            
-            cines_data.append(cine_data)
-    
-    return cines_data
+    pagination = soup.find('div', class_='pagination-item-holder')
+    if pagination:
+        last_page_element = pagination.find_all('a', class_='button')[-1]
+        total_pages = int(last_page_element.text.strip())
+    else:
+        total_pages = 1
 
-def get_peliculas_by_cine(cine_url):
-    
+    cinemas_data = []
+
+    # Iterate through all pages
+    for current_page in range(1, total_pages + 1):
+        # Request the current page
+        url = f"{base_url}?page={current_page}"
+        response = requests.get(url, headers=headers)
+
+        # Parse the page content with Beautiful Soup
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Find all cinema elements
+        cinemas = soup.find_all('div', class_='theater-card hred cf card-thumb-large')
+
+        for cinema in cinemas:
+            # Extract the cinema name
+            name_element = cinema.find('h3', class_='title')
+            name = name_element.text.strip() if name_element else "Name not available"
+
+            # Extract the cinema address
+            address_element = cinema.find('address', class_='address')
+            address = address_element.text.strip() if address_element else "Address not available"
+
+            # Extract the number of screens
+            screens_element = cinema.find('div', class_='screen-number')
+            screens = screens_element.text.strip() if screens_element else "Number of screens not available"
+
+            # Create a dictionary with the cinema data
+            cinema_data = {
+                "name": name,
+                "address": address,
+                "num_screens": screens
+            }
+
+            cinemas_data.append(cinema_data)
+
+    return cinemas_data
+
+def get_movies_by_cinema(cinema_url):
     # Fetches the URL of the cinema
-    cine_response = requests.get(cine_url)
+    cinema_response = requests.get(cinema_url)
 
-    peliculas_data = []
+    movies_data = []
     number_of_days = 1
 
-    if cine_response.status_code == 200:
-        cine_soup = BeautifulSoup(cine_response.content, 'lxml')
+    if cinema_response.status_code == 200:
+        cinema_soup = BeautifulSoup(cinema_response.content, 'lxml')
     
         for i in range(number_of_days):
             date = (datetime.now() + timedelta(days=i)).strftime('%Y-%m-%d')
-            date_url = cine_url + f"#shwt_date={date}"
+            date_url = cinema_url + f"#shwt_date={date}"
             
             date_response = requests.get(date_url)
             
             if date_response.status_code == 200:
                 date_soup = BeautifulSoup(date_response.content, 'lxml')
-                peliculas = date_soup.find_all('div', class_='card entity-card entity-card-list movie-card-theater cf hred')
+                movies = date_soup.find_all('div', class_='card entity-card entity-card-list movie-card-theater cf hred')
                 
-                if peliculas:
-                    for pelicula in peliculas:
-                        titulo_pelicula = pelicula.find('a', class_='meta-title-link')
-                        horarios = pelicula.find_all('span', class_='showtimes-hour-item-value')
+                if movies:
+                    for movie in movies:
+                        movie_title = movie.find('a', class_='meta-title-link')
+                        showtimes = movie.find_all('span', class_='showtimes-hour-item-value')
                         
-                        if titulo_pelicula and horarios:
-                            pelicula_data = {
-                                "titulo": titulo_pelicula.text.strip(),
-                                "horarios": [horario.text.strip() for horario in horarios],
-                                "fecha": date
+                        if movie_title and showtimes:
+                            movie_data = {
+                                "title": movie_title.text.strip(),
+                                "showtimes": [showtime.text.strip() for showtime in showtimes],
+                                "date": date
                             }
-                        peliculas_data.append(pelicula_data)
-        return peliculas_data
+                        movies_data.append(movie_data)
+        return movies_data
 
+@app.route('/cinemas', methods=['GET'])
+def get_cinemas():    
+    cinemas_data = get_cinemas_data()
+    if cinemas_data is None:
+        return jsonify({"error": "Could not retrieve cinema data"}), 500
+    return jsonify(cinemas_data)
 
-@app.route('/cines', methods=['GET'])
-def get_cines():    
-    cines_data = get_cines_data()
-    if cines_data is None:
-        return jsonify({"error": "No se pudieron obtener los datos de los cines"}), 500
-    return jsonify(cines_data)
-
-
-@app.route('/peliculas', methods=['GET'])
-def get_peliculas():
-    peliculas = []
+@app.route('/movies', methods=['GET'])
+def get_movies():
+    movies = []
     response = requests.get('https://www.sensacine.com/peliculas/')
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'lxml')
-        peliculas_list = soup.find_all('div', class_='entity-card')
-        for pelicula in peliculas_list:
-            titulo = pelicula.find('a', class_='meta-title-link').text.strip()
-            duracion = pelicula.find('div', class_='meta-body-item meta-body-info').text.strip()
-            director = pelicula.find('div', class_='meta-body-item meta-body-direction').text.strip()
-            reparto = pelicula.find('div', class_='meta-body-item meta-body-actor').text.strip()
-            sinopsis = pelicula.find('div', class_='content-txt').text.strip()
-            valoracion = pelicula.find('span', class_='stareval-note').text.strip()
-            pelicula_data = {
-                "titulo": titulo,
-                "duracion": duracion,
+        movies_list = soup.find_all('div', class_='entity-card')
+        for movie in movies_list:
+            title = movie.find('a', class_='meta-title-link').text.strip()
+            duration = movie.find('div', class_='meta-body-item meta-body-info').text.strip()
+            director = movie.find('div', class_='meta-body-item meta-body-direction').text.strip()
+            cast = movie.find('div', class_='meta-body-item meta-body-actor').text.strip()
+            synopsis = movie.find('div', class_='content-txt').text.strip()
+            rating = movie.find('span', class_='stareval-note').text.strip()
+            movie_data = {
+                "title": title,
+                "duration": duration,
                 "director": director,
-                "reparto": reparto,
-                "sinopsis": sinopsis,
-                "valoracion": valoracion
+                "cast": cast,
+                "synopsis": synopsis,
+                "rating": rating
             }
-            peliculas.append(pelicula_data)
-    return jsonify(peliculas)
+            movies.append(movie_data)
+    return jsonify(movies)
 
-@app.route('/horarios', methods=['GET'])
-def get_horarios():
-    cine_nombre = request.args.get('cine')
-    pelicula_titulo = request.args.get('pelicula')
+@app.route('/showtimes', methods=['GET'])
+def get_showtimes():
+    cinema_name = request.args.get('cinema')
+    movie_title = request.args.get('movie')
     
-    if not cine_nombre or not pelicula_titulo:
-        return jsonify({"error": "Parámetros 'cine' y 'pelicula' son requeridos"}), 400
+    if not cinema_name or not movie_title:
+        return jsonify({"error": "Parameters 'cinema' and 'movie' are required"}), 400
     
-    cines_data = get_cines_data()
-    if cines_data is None:
-        return jsonify({"error": "Error en la solicitud HTTP o no se encontraron cines"}), 500
+    cinemas_data = get_cinemas_data()
+    if cinemas_data is None:
+        return jsonify({"error": "HTTP request error or no cinemas found"}), 500
     
-    for cine in cines_data:
-        if cine["nombre"].lower() == cine_nombre.lower():
+    for cinema in cinemas_data:
+        if cinema["name"].lower() == cinema_name.lower():
 
-            cine_url = cine["url"]
-            peliculas = get_peliculas_by_cine(cine_url)
+            cinema_url = cinema["url"]
+            movies = get_movies_by_cinema(cinema_url)
 
-            for pelicula in peliculas:
-                if pelicula["titulo"].lower() == pelicula_titulo.lower():
+            for movie in movies:
+                if movie["title"].lower() == movie_title.lower():
                     return jsonify({
-                        "cine": cine["nombre"],
-                        "direccion": cine["direccion"],
-                        "titulo": pelicula["titulo"],
-                        "horarios": pelicula["horarios"],
-                        "fecha": pelicula["fecha"]
+                        "cinema": cinema["name"],
+                        "address": cinema["address"],
+                        "title": movie["title"],
+                        "showtimes": movie["showtimes"],
+                        "date": movie["date"]
                     })
     
-    return jsonify({"error": "No se encontraron horarios para la película en el cine especificado"}), 404
+    return jsonify({"error": "No showtimes found for the specified movie in the cinema"}), 404
 
+@app.route('/cinema_showtimes', methods=['GET'])
+def get_cinema_showtimes():
+    cinema_name = request.args.get('cinema')
 
-@app.route('/cartelera', methods=['GET'])
-def get_cartelera_by_cine():
-    cine_nombre = request.args.get('cine')
+    print(cinema_name)
+    
+    if not cinema_name:
+        return jsonify({"error": "Parameter 'cinema' is required"}), 400
+    
+    cinemas_data = get_cinemas_data()
+    if cinemas_data is None:
+        return jsonify({"error": "HTTP request error or no cinemas found"}), 500
+    
+    for cinema in cinemas_data:
+        if cinema["name"].lower() == cinema_name.lower():
+            print("Cinema found:")
+            print(cinema["name"] + " " + cinema["url"])
+            cinema_url = cinema["url"]
+            movies = get_movies_by_cinema(cinema_url)
+            return jsonify(movies)
+    
+    return jsonify({"error": "No showtimes found for the specified cinema"}), 404
 
-    print(cine_nombre)
+@app.route('/cinemas_movie', methods=['GET'])
+def get_cinemas_movie():
+    movie_title = request.args.get('movie')
     
-    if not cine_nombre:
-        return jsonify({"error": "Parámetro 'cine' es requerido"}), 400
+    if not movie_title:
+        return jsonify({"error": "Parameter 'movie' is required"}), 400
     
-    cines_data = get_cines_data()
-    if cines_data is None:
-        return jsonify({"error": "Error en la solicitud HTTP o no se encontraron cines"}), 500
+    cinemas_data = get_cinemas_data()
+    if cinemas_data is None:
+        return jsonify({"error": "HTTP request error or no cinemas found"}), 500
     
-    for cine in cines_data:
-        if cine["nombre"].lower() == cine_nombre.lower():
-            print("Cine encontrado:")
-            print(cine["nombre"] + " " + cine["url"])
-            cine_url = cine["url"]
-            peliculas = get_peliculas_by_cine(cine_url)
-            return jsonify(peliculas)
-    
-    return jsonify({"error": "No se encontraron horarios para el cine especificado"}), 404
-
-@app.route('/cines_pelicula', methods=['GET'])
-def get_cines_pelicula():
-    pelicula_titulo = request.args.get('pelicula')
-    
-    if not pelicula_titulo:
-        return jsonify({"error": "Parámetro 'pelicula' es requerido"}), 400
-    
-    cines_data = get_cine_data()
-    if cines_data is None:
-        return jsonify({"error": "Error en la solicitud HTTP o no se encontraron cines"}), 500
-    
-    cines_pelicula = []
-    for cine in cines_data:
-        for pelicula in cine["peliculas"]:
-            if pelicula["titulo"].lower() == pelicula_titulo.lower():
-                cines_pelicula.append({
-                    "cine": cine["nombre"],
-                    "direccion": cine["direccion"],
-                    "horarios": pelicula["horarios"],
-                    "fecha": pelicula["fecha"]
+    cinemas_movie = []
+    for cinema in cinemas_data:
+        for movie in cinema["movies"]:
+            if movie["title"].lower() == movie_title.lower():
+                cinemas_movie.append({
+                    "cinema": cinema["name"],
+                    "address": cinema["address"],
+                    "showtimes": movie["showtimes"],
+                    "date": movie["date"]
                 })
     
-    if not cines_pelicula:
-        return jsonify({"error": "No se encontraron cines que emitan la película especificada"}), 404
+    if not cinemas_movie:
+        return jsonify({"error": "No cinemas found showing the specified movie"}), 404
     
-    return jsonify(cines_pelicula)
+    return jsonify(cinemas_movie)
 
 @app.route('/static/<path:path>')
 def send_static(path):
